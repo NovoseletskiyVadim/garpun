8751217616c064d2ce34f830b86a144766ad98e5
Vadim <novoseletskiy.v@gmail.com>
2020-09-02 00:52:45 +0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const chokidar = require('chokidar');
+const FileType = require('file-type');
+require('dotenv').config();
+
+const eventWatcher = chokidar.watch(process.env.MEDIA_PATH, {
+  ignored: /^\./,
+  persistent: true,
+});
+
+eventWatcher
+  .on('add', (pathFile) => {
+    const fileMeta = getFileMeta(pathFile);
+    FileType.fromFile(pathFile).then((type) => {
+      if (type && type.ext === 'jpg' && typeof fileMeta === 'object') {
+
+        console.log('It\'s OK!!!'+pathFile);
+
+      } else {
+
+        console.error('WRONG_FILE_TYPE', pathFile);
+      }
+    });
+  })
+  .on('error', function (error) {
+    console.error('Error happened', error);
+  });
+
+module.exports = { eventWatcher };
