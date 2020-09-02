6032878e83dcc0dd6261b8a65ad59298ae619793
Vadim <novoseletskiy.v@gmail.com>
2020-09-02 23:36:55 +0300

    Refactoring app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const chokidar = require('chokidar');
-const FileType = require('file-type');
 require('dotenv').config();
 
-const LogRegExp = require('./utils/logerRegExp')
+const watcherEventAdd =require('./events/watcherEventAdd');
+
+
+
+
+
 
 const eventWatcher = chokidar.watch(process.env.MEDIA_PATH, {
   ignored: /^\./,
@@ -14,23 +18,8 @@ const eventWatcher = chokidar.watch(process.env.MEDIA_PATH, {
 eventWatcher
   .on('add', (pathFile) => {
 
-    FileType.fromFile(pathFile).then((type) => {
-
-      const patternCheckNameFile=/(\d{17})_([a-zA-Z0-9А-Яа-я]{4,8})_(VEHICLE_DETECTION)/;
-      const RegExpArray=pathFile.match(patternCheckNameFile);
-
-      if (type && type.ext === 'jpg') {
-        
-        LogRegExp.saveDetectEvent(RegExpArray);
-
-      } else {
-
-        const err='WRONG_FILE_TYPE'     
-
-        LogRegExp.saveErrorEvent(RegExpArray,err);
-      
-      }
-    });
+    watcherEventAdd.eventWatchAddNewFile(pathFile);
+    
   })
   .on('error', function (error) {
     console.error('Error happened', error);
