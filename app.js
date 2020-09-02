eaa941cb5c7bbd579d9c5a5e90425257643dffd7
Vadim <novoseletskiy.v@gmail.com>
2020-09-02 13:30:26 +0300

    Start work with logger

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const chokidar = require('chokidar');
 const FileType = require('file-type');
 require('dotenv').config();
 
+const LogRegExp = require('./utils/logerRegExp')
+
 const eventWatcher = chokidar.watch(process.env.MEDIA_PATH, {
   ignored: /^\./,
   persistent: true,
@@ -15,8 +17,21 @@ eventWatcher
     FileType.fromFile(pathFile).then((type) => {
 
       if (type && type.ext === 'jpg') {
+        
+        // console.log('It\'s OK!!!'+pathFile);
+        // const nameFile=pathFile.split("\\").pop();
+        // console.log('nameFile='+nameFile+' typeoF= '+typeof nameFile);
 
-        console.log('It\'s OK!!!'+pathFile);
+        const patternCheckNameFile=/(\d{17})_([a-zA-Z0-9А-Яа-я]{4,8})_(VEHICLE_DETECTION)/;
+
+
+        const RegExpArray=pathFile.match(patternCheckNameFile);
+
+        console.log(RegExpArray);
+        
+
+
+
 
       } else {
 
