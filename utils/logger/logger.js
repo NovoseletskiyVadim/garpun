1de50ddfa046c9d2646f8c3f3bc267f8cf7d18ad
Vadim <novoseletskiy.v@gmail.com>
2020-09-04 00:24:00 +0300

    Create logger.js

---
+++ utils/logger/logger.js
@@ -0,0 +1,35 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const saveAppError = (eventData) => {
+  const { message } = eventData;
+  const stream = fs.createWriteStream(
+    path.join(__dirname, '../logs/error.log'),
+    { flags: 'a' }
+  );
+  stream.write(
+    `${JSON.stringify({
+      time: new Date(),
+      message,
+    })}\n`
+  );
+  stream.end();
+};
+
+const rejectFileLog = (eventData) => {
+  const stream = fs.createWriteStream(
+    path.join(__dirname, '../logs/reject.log'),
+    { flags: 'a' }
+  );
+  stream.write(
+    `${JSON.stringify({
+      time: new Date(),
+      message: eventData.message,
+      file: eventData.file,
+    })}\n`
+  );
+  stream.end();
+};
+
+module.exports = { rejectFileLog, saveAppError };
