811f650fcc817f1a61eb7d0e8d30363d50bf9f68
Vadim <novoseletskiy.v@gmail.com>
2020-09-04 23:38:33 +0300

    Check..

--- utils/watcher/watcherEventAddNewFile.js
+++ utils/watcher/watcherEventAddNewFile.js
@@ -1,32 +1,42 @@
-const getFileMeta = require('./getFileMeta');
 const FileType = require('file-type');
+const getFileMeta = require('./getFileMeta');
 const eventHandler = require('../handlers/eventHandler');
 const appLogger = require('../logger/logger');
-const rejectFileHandler = require('./utils/rejectFileHandler');
+const rejectFileHandler = require('../handlers/rejectFileHandler');
 // TODO: change path file
 // require('./db/dbConnect');
 
 
 
 
-const watcherEventAddNewFile=function(){
+const watcherEventAddNewFile=function(pathFile){
 
-    
     const fileMeta = getFileMeta(pathFile);
+
     FileType.fromFile(pathFile).then((type) => {
+        
         if (!type || type.ext !== 'jpg') {
+
             fileMeta.isValid = false;
             fileMeta.notPassed.push('FILE_TYPE');
+
         }
         if (fileMeta.isValid) {
+            
             eventHandler(fileMeta);
+
         } else {
+            
             appLogger.rejectFileLog({
+
                 message: fileMeta.notPassed.join(),
-                file: fileMeta.file,
+                // FIXME:fileMeta.file>fileMeta.file.fullPath
+                file: fileMeta.file.fullPath,
+                
             });
-        rejectFileHandler(pathFile);
-        console.error('WRONG_FILE', pathFile);
+
+            rejectFileHandler(pathFile);
+            // console.error('WRONG_FILE', pathFile);
         }
     });
 
