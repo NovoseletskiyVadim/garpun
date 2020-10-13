3867ae7752330cb4747c886fdc8080dceffeda3f
BuianAlex <a.buyan@urk.net>
2020-10-13 19:15:45 +0300

    down to serw

--- garpunFrontServer/public/javascripts/script.js
+++ garpunFrontServer/public/javascripts/script.js
@@ -1,5 +1,5 @@
 'use strict';
-const socket = io('localhost:8888');
+const socket = io('10.15.1.235:8888');
 const eventListWrapper = document.getElementById('events-monitor');
 
 const showBigImg = (target) => {
